ca5bc834e219be1cac239276084340f6c5519ebf
Syk-i <34940095+Syk-i@users.noreply.github.com>
2019-01-27 20:13:14 +0000

    Added new chest object with working script

---
+++ Dungeon V1/Assets/Scripts/Chest/ChestInteract.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestInteract : CoinManager
+{
+    public GameObject coins;
+    public int CoinsToGive;
+    public GameObject Chest;
+
+    public void Start()
+    {
+        //CoinsToGive= coins.GetComponent<CoinManager>().scoreValue;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            scoreValue = PlayerPrefs.GetInt("PlayerAmount");
+            scoreValue += 2;
+            GameObject.Destroy(Chest);
+            PlayerPrefs.SetInt("PlayerCoinAmount", scoreValue);
+            PlayerPrefs.Save();
+            
+        }
+    }
+}
