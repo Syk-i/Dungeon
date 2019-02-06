ff699354f3e5d456ac0ced5f189c1ca3ea286324
Syk-i <34940095+Syk-i@users.noreply.github.com>
2019-02-06 12:59:26 +0000

    Added GameOver Overlay Screen
    Linked to Master Object
    Need to Connect to Start Menu Screen

---
+++ Dungeon V1/Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+    public GameObject GameOverScreenOverlay;
+    private int PlayerHealth;
+	// Use this for initialization
+	void Start () {
+         // PlayerHealth = PlayerPrefs.GetInt("PlayerHealth");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerHealth = PlayerPrefs.GetInt("PlayerHealth");
+        if (PlayerHealth <= 0)
+        {
+            GameOverScreenOverlay.SetActive(true);
+        }
+	}
+}
